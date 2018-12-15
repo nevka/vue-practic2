254889e27e598dc338bfab4e6bf72a40a0498a07
Алина <nevkaa@yandex.ru>
2018-12-15 18:13:59 +0300

    58

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as fb from 'firebase/app'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -15,5 +16,16 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    var config = {
+      apiKey: 'AIzaSyD0GL_YLRm8yixk237TPvhzrC2Ab9YJp1g',
+      authDomain: 'vue-practic2.firebaseapp.com',
+      databaseURL: 'https://vue-practic2.firebaseio.com',
+      projectId: 'vue-practic2',
+      storageBucket: 'vue-practic2.appspot.com',
+      messagingSenderId: '729556122653'
+    }
+    fb.initializeApp(config)
+  }
 })
