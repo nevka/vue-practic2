eb806e03062815d2f8d6dd2a8e6060f547b2311f
Алина <nevkaa@yandex.ru>
2018-12-17 20:14:47 +0300

    65

--- src/components/Products/NewProduct.vue
+++ src/components/Products/NewProduct.vue
@@ -14,26 +14,27 @@
         </v-form>
         <v-layout class="mb-3">
           <v-flex xs12>
-            <v-btn class="warning">
+            <v-btn class="warning" @click="upload">
               Загрузить
               <v-icon right dark>cloud_upload</v-icon>
             </v-btn>
+            <input type="file" style="display: none;" accept="image/*" ref="fileInput" @change="onFileChange">
           </v-flex>
         </v-layout>
         <v-layout>
           <v-flex xs12>
-            <img src="" height="200px">
+            <img :src="imageSrc" height="200px" v-if="imageSrc">
           </v-flex>
         </v-layout>
         <v-layout>
           <v-flex xs12>
-            <v-switch color="primery" v-model="promo" label="Добавить в промо"></v-switch>
+            <v-switch color="primary" v-model="promo" label="Добавить в промо"></v-switch>
           </v-flex>
         </v-layout>
         <v-layout>
           <v-flex xs12>
             <v-spacer></v-spacer>
-            <v-btn class="success" :loading="loading" :disabled="!valid || loading" @click="createProduct">Создать продукт</v-btn>
+            <v-btn class="success" :loading="loading" :disabled="!valid || !image || loading" @click="createProduct">Создать продукт</v-btn>
           </v-flex>
         </v-layout>
       </v-flex>
@@ -52,7 +53,9 @@
         price: 0,
         description: '',
         promo: false,
-        valid: false
+        valid: false,
+        image: null,
+        imageSrc: ''
       }
     },
     computed: {
@@ -62,7 +65,7 @@
     },
     methods: {
       createProduct () {
-        if (this.$refs.form.validate()) {
+        if (this.$refs.form.validate() && this.image) {
           const product = {
             title: this.title,
             vendor: this.vendor,
@@ -71,7 +74,7 @@
             price: this.price,
             description: this.description,
             promo: this.promo,
-            valid: this.valid
+            image: this.image
           }
           this.$store.dispatch('createProduct', product)
             .then(() => {
@@ -79,6 +82,18 @@
             })
             .catch(() => {})
         }
+      },
+      upload () {
+        this.$refs.fileInput.click()
+      },
+      onFileChange (event) {
+        const file = event.target.files[0]
+        const reader = new FileReader()
+        reader.onload = e => {
+          this.imageSrc = reader.result
+        }
+        reader.readAsDataURL(file)
+        this.image = file
       }
     }
   }
--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -31,7 +31,7 @@ export default {
     async createProduct ({commit, getters}, payload) {
       commit('clearError')
       commit('setLoading', true)
-      // const image = payload.image
+      const image = payload.image
       try {
         const newProduct = new Product(
           payload.title,
@@ -45,15 +45,15 @@ export default {
           payload.promo)
 
         const product = await fb.database().ref('products').push(newProduct)
-        // const imageExt = image.name.slice(image.name.lastIndexOf('.'))
-        // const fileData = await fb.storage().ref(`products/${product.key}.${imageExt}`).put(image)
-        // const imageSrc = await fb.storage().ref().child(fileData.ref.fullPath).getDownloadURL()
-        // await fb.database().ref('products').child(product.key).update({ imageSrc })
+        const imageExt = image.name.slice(image.name.lastIndexOf('.'))
+        const fileData = await fb.storage().ref(`products/${product.key}.${imageExt}`).put(image)
+        const imageSrc = await fb.storage().ref().child(fileData.ref.fullPath).getDownloadURL()
+        await fb.database().ref('products').child(product.key).update({ imageSrc })
         commit('setLoading', false)
         commit('createProduct', {
           ...newProduct,
-          id: product.key
-          // imageSrc
+          id: product.key,
+          imageSrc
         })
       } catch (error) {
         commit('setError', error.message)
